631bdf1492b21ecc1da71dfc7b43fecd89cba721
Zaheer <thisiszaheer.786@gmail.com>
2023-06-01 09:04:04 +0530

    Doing Automation, with automation task becoming easy

--- 04-function.sh
+++ 04-function.sh
@@ -3,3 +3,13 @@ print_hello()
   echo Hello DevOps Engineer
 }
 print_hello
+
+
+print_arguments(){
+  echo First argument $1
+  echo Second argument $2
+  echo All arguments $*
+  echo No.of arguments $#
+}
+
+print_arguments abc 123 xyz 456 Qwerty 7890
