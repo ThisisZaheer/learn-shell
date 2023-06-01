15bcfd58d9b8836e78d32ca72d1a95598ffb08d1
Zaheer <thisiszaheer.786@gmail.com>
2023-06-01 08:18:53 +0530

    Doing Automation, with automation task becoming easy

--- 03-var.sh
+++ 03-var.sh
@@ -9,3 +9,10 @@ b=89777
 echo b value is $bMynumber
 
 echo b value is ${b}Mynumber
+
+#special variables input numbers are $0 to $n, $*, $#
+
+echo Script name - $0
+echo First argument - $1
+echo All arguments - $*
+echo No.of arguments - $#
