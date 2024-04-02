ffeeda8a6f254c8d7d09ca6f78ac5df7c135fab1
Zaheer <thisiszaheer.786@gmail.com>
2024-04-02 15:24:47 +0530

    sample Add function for Script

--- 04-function.sh
+++ 04-function.sh
@@ -1,4 +1,23 @@
+# Function
 sample1() {
+  # Inside Function Code
   echo "Hello DevOps"
 }
+
+# Main Program
 sample1
+
+# Declared Variable in main program can be accessed in function and it is vice-versa
+# Declared Variable in main program can be overwritten by function and it is vice-versa
+
+# Along with this, function have special variables, we can pass arguments to functions
+
+# sample1 100 200
+
+sample_sum() {
+
+  echo "Addition of $1 and $2 is $(($1+$2))"
+
+}
+
+sample_sum 175 375
